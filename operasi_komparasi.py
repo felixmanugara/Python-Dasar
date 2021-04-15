82be40501add2c6ab3823e9ea181245a1b3cd2d5
Felix Manugara <marcofel.manugara@gmail.com>
2021-04-15 12:20:18 +0700

    belajar operasi komparasi (perbandingan)

---
+++ operasi_komparasi.py
@@ -0,0 +1,56 @@
+# operasi komparasi, setiap hasil dari komparasi adalah boolean
+# <, >, <=, >=, ==, !=, is, is not
+
+a = 5
+b = 3
+
+# operasi komparasi kurang dari <
+print("====== operasi komparasi kurang dari < ========")
+result = a < 6
+print(a,' < ',6 ,'=', result)
+result = a < 4
+print(a,' < ',4 ,'=', result)
+result = a < 5
+print(a,' < ',5 ,'=', result)
+
+# operasi komparasi lebih dari >
+print("======= operasi komparasi lebih besar dari > ========")
+result = a > 6
+print(a,' > ',6 ,'=', result)
+result = a > 4
+print(a,' > ',4 ,'=', result)
+result = a > 5
+print(a,' > ',5 ,'=', result)
+
+print("======= operasi komparasi lebih kecil sama dengan <= ========")
+result = a <= 6
+print(a,' <= ',6 ,'=', result)
+result = a <= 4
+print(a,' <= ',4 ,'=', result)
+result = a <= 5
+print(a,' <= ',5 ,'=', result)
+
+print("======= operasi komparasi lebih besar sama dengan >= ========")
+result = a >= 6
+print(a,' >= ',6 ,'=', result)
+result = a >= 4
+print(a,' >= ',4 ,'=', result)
+result = a >= 5
+print(a,' >= ',5 ,'=', result)
+
+print("======= operasi komparasi sama dengan == ========")
+result = a == 6
+print(a,' == ',6 ,'=', result)
+result = a == 4
+print(a,' == ',4 ,'=', result)
+result = a == 5
+print(a,' == ',5 ,'=', result)
+
+print("======= operasi komparasi tidak sama dengan != ========")
+result = a != 6
+print(a,' != ',6 ,'=', result)
+result = a != 4
+print(a,' != ',4 ,'=', result)
+result = a != 5
+print(a,' != ',5 ,'=', result)
+

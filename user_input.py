b5998d4dc42a26ecaa343d7a8793eba73f4bf41c
Felix Manugara <marcofel.manugara@gmail.com>
2021-04-15 12:17:22 +0700

    pengunaan fungsi input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# mengambil input dari user menggunakan input()
+
+data1 = input("masukkan nama: ") # memasukkan fungsi input kedalam variabel bernama data1
+data2 = input("masukkan umur: ")
+print("nama kamu adalah: ", data1)
+print("umur kamu adalah: ", data2)# output berupa umur kamu adalah berserta data yang telah dimasukkan user

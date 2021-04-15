9ee026850db83949bd251a818c915ecb21befc9a
Felix Manugara <marcofel.manugara@gmail.com>
2021-04-15 11:51:09 +0700

    berisi tentang cara penulisan variabel

---
+++ variabel.py
@@ -0,0 +1,16 @@
+# belajar variabel
+# variabel adalah sebuah wadah untuk menampung data
+
+# contoh di bawah merupakan a sebagai variabel 15 adalah value
+# contoh assignment nilai
+a = 15
+y = 68
+data = 200
+
+# perintah print digunakan untuk memanggil data
+print(a)
+
+# aturan penamaan dalam python
+nilai_x = 80 # penamaan variabel tidak boleh menggunakan spasi dan angka diawal nama variabel
+data1 = 34 # ini boleh
+variabelq = 6.5 # ini boleh
